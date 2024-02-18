67ec96991d50dd0668a3c345b25ec548f936de0b
topaccina <54760952+topaccina@users.noreply.github.com>
2024-02-18 17:53:19 +0100

    HF_imageToText test

---
+++ app.py
@@ -0,0 +1,27 @@
+from transformers import pipeline
+import glob
+import pandas as pd
+import warnings
+from dotenv import load_dotenv, find_dotenv
+
+load_dotenv(find_dotenv())
+
+warnings.filterwarnings("ignore")
+
+# specify task and model
+pipe = pipeline("image-to-text", model="Salesforce/blip-image-captioning-large")
+# get all the pngs to process
+pngList = glob.glob("./png/*.png")
+# call the model
+response = pipe(pngList)
+# parse the output. Create a dataframe w/ generated captions for each png
+generated_text = []
+for resp in response:
+    generated_text.append(resp[0]["generated_text"])
+    print(resp[0]["generated_text"])
+
+
+output = pd.DataFrame({"pngFileName": pngList, "Caption": generated_text})
+
+# export to a csv
+output.to_csv("./output/generated_text.csv", index=False)
